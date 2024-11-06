ab6f363905712bd0f2bfc3c8ab34cf9d9476a3fb
Cris John Cañales <crisjohn.canales@gmail.com>
2024-11-06 20:15:11 +0800

    PostItem Components

---
+++ src/components/PostItemComponents.vue
@@ -0,0 +1,40 @@
+<script setup>
+defineProps({
+  post: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <!-- Card -->
+  <div class="bg-slate-100 rounded-t-lg overflow-hidden shadow-md mb-8">
+    <!--Card Header -->
+    <div class="bg-slate-500 flex justify-between items-center p-2">
+      <div>
+        <h1 class="text-slate-50">{{ post.title }}</h1>
+        <p class="text-xs text-slate-200">{{ post.author }} - {{ post.created_at }}</p>
+      </div>
+      <!-- action -->
+      <div class="flex justify-between items-center gap-2">
+        <button class="save material-icons text-blue-500 p-1 rounded-full bg-white">
+          bookmark_border
+        </button>
+        <button class="del material-icons text-red-500 p-1 bg-white rounded-full">delete</button>
+      </div>
+    </div>
+
+    <!--Card Body -->
+    <div class="p-4">
+      {{ post.body }}
+    </div>
+  </div>
+  <button
+    class="bg-white text-black border border-black px-4 py-2 rounded hover:bg-gray-800 hover:text-white"
+  >
+    Button
+  </button>
+</template>
+
+<style lang="scss" scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,6 @@
 <script setup>
+import PostItemComponents from '@/components/PostItemComponents.vue'
+
 const posts = [
   {
     id: 1,
@@ -44,35 +46,8 @@ const posts = [
 </script>
 
 <template>
-  <!-- Card -->
-  <div
-    v-for="post in posts"
-    :key="post.id"
-    class="bg-slate-100 rounded-t-lg overflow-hidden shadow-md mb-8"
-  >
-    <!--Card Header -->
-    <div class="bg-slate-500 flex justify-between items-center p-2">
-      <div>
-        <h1 class="text-slate-50">{{ post.title }}</h1>
-        <p class="text-xs text-slate-200">{{ post.author }} - {{ post.created_at }}</p>
-      </div>
-      <!-- action -->
-      <div class="flex justify-between items-center gap-2">
-        <button class="save material-icons text-blue-500 p-1 rounded-full bg-white">
-          bookmark_border
-        </button>
-        <button class="del material-icons text-red-500 p-1 bg-white rounded-full">delete</button>
-      </div>
-    </div>
-
-    <!--Card Body -->
-    <div class="p-4">
-      {{ post.body }}
-    </div>
+  <div v-for="post in posts" :key="post.id">
+    <PostItemComponents :post="post" />
+    <!-- attribute binding ^ -->
   </div>
-  <button
-    class="bg-white text-black border border-black px-4 py-2 rounded hover:bg-gray-800 hover:text-white"
-  >
-    Button
-  </button>
 </template>
