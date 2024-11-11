df80613aab95a523ec1c305cbc284afe8af69e95
Cris John Cañales <crisjohn.canales@gmail.com>
2024-11-11 19:22:59 +0800

    form submittion with computed

--- src/views/PostCreateView.vue
+++ src/views/PostCreateView.vue
@@ -1,17 +1,27 @@
 <script setup>
-import { reactive } from 'vue'
+import { reactive, computed } from 'vue'
 import CardCompoenents from '@/components/CardCompoenents.vue'
 
+// reactive
 const post = reactive({
-  title: 'Hello World',
+  title: '',
   body: '',
 })
+// computed
+const isFormInvalid = computed(() => {
+  return post.title === '' || post.body === ''
+})
+
+// methods
+const formSubmit = () => {
+  console.log(post)
+}
 </script>
 
 <template>
   <CardCompoenents>
     <div class="px-3 py-4">
-      <form action="">
+      <form @submit.prevent="formSubmit">
         <h3 class="mb-8">Create new Post</h3>
         <div class="mb-3">
           <label class="block">Title</label>
@@ -19,10 +29,17 @@ const post = reactive({
         </div>
         <div class="mb-3">
           <label class="block">Body</label>
-          <textarea v-model="post.title" class="form-control"></textarea>
+          <!-- Corrected v-model binding -->
+          <textarea v-model="post.body" class="form-control"></textarea>
         </div>
         <div class="mb-5">
-          <button class="p-2 bg-slate-500 w-full rounded-md text-white">Add</button>
+          <button
+            class="p-2 bg-slate-500 w-full rounded-md text-white"
+            :disabled="isFormInvalid"
+            :class="isFormInvalid && 'bg-slate-400 cursor-not-allowed'"
+          >
+            Add
+          </button>
         </div>
       </form>
     </div>
