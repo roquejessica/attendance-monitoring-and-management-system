8e657efde70b930d26b250020c07311c6ee28df2
Cris John Cañales <crisjohn.canales@gmail.com>
2024-11-11 18:57:02 +0800

    create post form

--- src/views/PostCreateView.vue
+++ src/views/PostCreateView.vue
@@ -1,5 +1,30 @@
-<script setup></script>
+<script setup>
+import { reactive } from 'vue'
+import CardCompoenents from '@/components/CardCompoenents.vue'
+
+const post = reactive({
+  title: 'Hello World',
+  body: '',
+})
+</script>
 
 <template>
-  <div>Post Create Page</div>
+  <CardCompoenents>
+    <div class="px-3 py-4">
+      <form action="">
+        <h3 class="mb-8">Create new Post</h3>
+        <div class="mb-3">
+          <label class="block">Title</label>
+          <input type="text" v-model="post.title" class="form-control" />
+        </div>
+        <div class="mb-3">
+          <label class="block">Body</label>
+          <textarea v-model="post.title" class="form-control"></textarea>
+        </div>
+        <div class="mb-5">
+          <button class="p-2 bg-slate-500 w-full rounded-md text-white">Add</button>
+        </div>
+      </form>
+    </div>
+  </CardCompoenents>
 </template>
