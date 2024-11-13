e328ea45bd4a482944ce302f0b555f1565b67719
Cris John Cañales <crisjohn.canales@gmail.com>
2024-11-13 18:09:00 +0800

    vue 3 with pinia state

--- src/main.js
+++ src/main.js
@@ -8,6 +8,6 @@ import router from './router'
 const app = createApp(App) //creating variable app and gi inside ang App parameter. createApp from vue package
 
 app.use(createPinia()) //register create Pinia pack
-app.use(router) //routes is plugin on index registered router. Used for create app
+app.use(router) //routes is plugin on index registered router. Used for create app, a package from vue
 
 app.mount('#app') //mount is a method, #app finding index.html, mount plugins to index.html
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,62 +1,21 @@
 <script setup>
-// import CardComponents from '@/components/CardComponents.vue'
-import PostItemComponents from '@/components/PostItemComponents.vue'
+// import PostItemComponents from '@/components/PostItemComponents.vue'
+import CardComponents from '@/components/CardComponents.vue'
+import { usePostsStore } from '@/stores/post'
 
-let posts = [
-  {
-    id: 1,
-    title: 'Sample Post 1',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    author: 'Pedro Penduco',
-    created_at: '10/06/2024',
-    is_saved: false,
-  },
-  {
-    id: 2,
-    title: 'Sample Post 2',
-    body: 'Sed non velit velit. Integer nec odio metus. Donec ut turpis ac libero pharetra gravida. Fusce efficitur lectus quis ante dapibus, ut interdum lorem fermentum. Vivamus suscipit, turpis ut cursus tincidunt, elit augue tincidunt magna, ac scelerisque felis lorem non arcu.',
-    author: 'Pedro Penduco',
-    created_at: '10/06/2024',
-    is_saved: false,
-  },
-  {
-    id: 3,
-    title: 'Sample Post 3',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    author: 'Pedro Penduco',
-    created_at: '10/08/2024',
-    is_saved: false,
-  },
-  {
-    id: 4,
-    title: 'Sample Post 4',
-    body: 'Mauris in nisl et tortor tempor accumsan in id felis. Curabitur tincidunt magna in augue luctus, sit amet gravida enim posuere. Sed non velit velit. Integer nec odio metus. Donec ut turpis ac libero pharetra gravida.',
-    author: 'Pedro Penduco',
-    created_at: '10/08/2024',
-    is_saved: false,
-  },
-  {
-    id: 5,
-    title: 'Sample Post 5',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    author: 'Pedro Penduco',
-    created_at: '10/10/2024',
-    is_saved: false,
-  },
-]
-
+const postStore = usePostsStore()
+console.log(postStore)
 // methods
-const deletePost = (id) => {
-  posts = posts.filter((p) => p.id !== id)
+// const deletePost = (id) => {
+//   posts = posts.filter((p) => p.id !== id)
 
-  console.log(posts)
-}
+//   console.log(posts)
+// }
 </script>
 
 <template>
-  <div v-for="post in posts" :key="post.id">
-    <!-- <CardComponents>
-
+  <div v-for="post in postStore.posts" :key="post.id">
+    <CardComponents>
       <template v-slot:card-header>
         <div>
           <h1 class="text-slate-50">{{ post.title }}</h1>
@@ -71,12 +30,11 @@ const deletePost = (id) => {
         </div>
       </template>
 
-
       <div class="p-4">
         {{ post.body }}
       </div>
-    </CardComponents> -->
+    </CardComponents>
 
-    <PostItemComponents :post="post" @get-id="deletePost" />
+    <!-- <PostItemComponents :post="post" @get-id="deletePost" /> -->
   </div>
 </template>
